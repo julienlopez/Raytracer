701fb16417b0b197eea1aa9eb52d6ee192b17f0b
julien lopez <julien.lopez51@gmail.com>
2017-06-02 10:39:41 +0200

    creating basic GUI to display the results (#1)

--- lib/math/types.cpp
+++ lib/math/types.cpp
@@ -13,4 +13,13 @@ Eigen::Vector3d normalize(const Eigen::Vector3d& v)
     return v / std::sqrt(norm2(v));
 }
 
+Eigen::Vector3d createPoint(const double x, const double y, const double z)
+{
+    Eigen::Vector3d res;
+    res(0) = x;
+    res(1) = y;
+    res(2) = z;
+    return res;
+}
+
 } // Math
